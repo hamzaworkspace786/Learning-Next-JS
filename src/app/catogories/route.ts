d67cfa8546b599c2ecf932b8ddb87a53af4eef18
Hamza <hamzasworkspace1@gmail.com>
2026-01-05 20:32:25 +0500

    Caching Done

--- src/app/catogories/route.ts
+++ src/app/catogories/route.ts
@@ -1,2 +1,14 @@
+import { NextResponse } from "next/server";
+export const dynamic = 'force-static';
+
 // Making sense of caching 
-// dasdasda
+export async function GET() {
+    // This data typically comes from a database or an API adn it does not need to be refreshed on every request.so we can cache it.
+    const categories= [
+            { id: 1, name: 'Electronics' },
+            { id: 2, name: 'Books' },
+            { id: 3, name: 'Clothing' },
+            { id: 4, name: 'Home & Kitchen' },
+        ];
+        return  NextResponse.json(categories)
+    }
--- src/app/time/route.ts
+++ src/app/time/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
+export const dynamic = 'force-static';
+export const revalidate = 10; // Revalidate the data every 10 seconds
 
-// Caching 
+
+// Caching only works with GET methods
 export async function GET()
 {
     return NextResponse.json({time : new Date().toLocaleTimeString()})
